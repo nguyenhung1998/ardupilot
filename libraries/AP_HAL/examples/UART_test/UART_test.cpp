d8cddd3bb35ba2cfea3159d9b2ffb9579f8220a6
Nguyen Hung <nguyenhungblog@gmail.com>
2018-11-28 22:26:59 +0700

    UART Test for GPS

--- libraries/AP_HAL/examples/UART_test/UART_test.cpp
+++ libraries/AP_HAL/examples/UART_test/UART_test.cpp
@@ -1,8 +1,12 @@
 /*
   simple test of UART interfaces
  */
-
 #include <AP_HAL/AP_HAL.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <TinyGPS/TinyGPS++.h>
 
 #if HAL_OS_POSIX_IO
 #include <stdio.h>
@@ -13,6 +17,8 @@ void loop();
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
+// The TinyGPS++ object
+TinyGPSPlus gps;
 /*
   setup one UART at 57600
  */
@@ -22,7 +28,7 @@ static void setup_uart(AP_HAL::UARTDriver *uart, const char *name)
         // that UART doesn't exist on this platform
         return;
     }
-    uart->begin(57600);
+    uart->begin(115200);
 }
 
 
@@ -35,37 +41,24 @@ void setup(void)
     hal.scheduler->delay(1000); //Ensure that the uartA can be initialized
 
     setup_uart(hal.uartA, "uartA");  // console
-    setup_uart(hal.uartB, "uartB");  // 1st GPS
-    setup_uart(hal.uartC, "uartC");  // telemetry 1
-    setup_uart(hal.uartD, "uartD");  // telemetry 2
-    setup_uart(hal.uartE, "uartE");  // 2nd GPS
-}
-
-static void test_uart(AP_HAL::UARTDriver *uart, const char *name)
-{
-    if (uart == nullptr) {
-        // that UART doesn't exist on this platform
-        return;
-    }
-    uart->printf("Hello on UART %s at %.3f seconds\n",
-                 name, (double)(AP_HAL::millis() * 0.001f));
+    setup_uart(hal.uartB, "uartB");  // console
+//    setup_uart(hal.uartB, "uartB");  // 1st GPS
+//    setup_uart(hal.uartC, "uartC");  // telemetry 1
+//    setup_uart(hal.uartD, "uartD");  // telemetry 2
+//    setup_uart(hal.uartE, "uartE");  // 2nd GPS
 }
 
 void loop(void)
 {
-    test_uart(hal.uartA, "uartA");
-    test_uart(hal.uartB, "uartB");
-    test_uart(hal.uartC, "uartC");
-    test_uart(hal.uartD, "uartD");
-    test_uart(hal.uartE, "uartE");
-
-    // also do a raw printf() on some platforms, which prints to the
-    // debug console
-#if HAL_OS_POSIX_IO
-    ::printf("Hello on debug console at %.3f seconds\n", (double)(AP_HAL::millis() * 0.001f));
-#endif
-
-    hal.scheduler->delay(1000);
+	while(hal.uartB->available())
+	{
+		if (gps.encode((char)hal.uartB->read()))
+		//char inChar = (char)hal.uartB->read();
+		if (gps.location.isValid())
+		{
+			hal.uartA->printf("%4.6f, %4.6f " , gps.location.lat(), gps.location.lng());
+		}
+	}
 }
 
 AP_HAL_MAIN();
